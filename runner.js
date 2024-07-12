50ef6df30a9d33c023f1d07ea10ede4e48e5653f
Alisson T. Bucchi <162882339+alisson-t-bucchi@users.noreply.github.com>
2024-07-12 22:58:34 +0100

    Create runner.js
    Integrando Tesults.

---
+++ runner.js
@@ -0,0 +1,16 @@
+const cypress = require('cypress')
+const tesults = require('./cypress-tesults-reporter');
+
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImM4YmU3YTk0LWFhYTAtNDhjNC1iZDBmLWIwMDI2ODU5YTYxOC0xNzIwODIwNzU3NzkwIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiYTgyY2I3MDUtNTA4YS00ZGJkLWI0NjMtNDAwMDE5ZGVlYjU3IiwidHlwZSI6InQifQ.FNZeXhOMONGK428TSiXPKAQwDQkNyTbXCT9th8Dt0N4'
+
+cypress.run({
+})
+.then((results) => {
+  const args = {
+      target: TOKEN,
+  }
+  tesults.results(results, args);
+})
+.catch((err) => {
+  console.error(err)
+})
